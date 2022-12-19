33174ba00957058d70ba83f018a1028476309c0d
adrianizq <adrianizq@gmail.com>
2022-12-19 18:57:13 -0500

    eureka server

---
+++ registry-eureka/src/test/java/itp/instituo/registryeureka/RegistryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package itp.instituo.registryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
