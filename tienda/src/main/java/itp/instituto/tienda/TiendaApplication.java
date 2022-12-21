b1d7e47af0981f2781f63079ca27bee5f33f9f30
adrianizq <adrianizq@gmail.com>
2022-12-21 17:47:11 -0500

    configurando los demas servicios para que se integren con eureka

--- tienda/src/main/java/itp/instituto/tienda/TiendaApplication.java
+++ tienda/src/main/java/itp/instituto/tienda/TiendaApplication.java
@@ -2,7 +2,9 @@ package itp.instituto.tienda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class TiendaApplication {
 
