951ffd8cdd83f68226bd534ecd58b2ca51812562
adrianizq <adrianizq@gmail.com>
2022-12-20 19:19:27 -0500

    registrando customer en eureka

--- customer/src/main/java/itp/instituto/customer/CustomerApplication.java
+++ customer/src/main/java/itp/instituto/customer/CustomerApplication.java
@@ -2,7 +2,9 @@ package itp.instituto.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CustomerApplication {
 
